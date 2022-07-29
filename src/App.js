e7be69711b2ea89ce61d41ab8a26f214fde0f1bf
nohhow <xksrma97@gmail.com>
2022-07-29 23:52:55 +0900

    [💄style] : 타이틀 폰트 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Cute from "./components/Cute";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-blue-100">
-      <h1 className="text-2xl font-monos mb-5">행복한 하루</h1>
+      <h1 id="top-title" className="text-2xl font-monos mb-5">행복한 하루</h1>
       <div className="flex flex-row justify-center">
         <div>
         <Weather/>
