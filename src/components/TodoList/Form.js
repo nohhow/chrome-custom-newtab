a94185d6fcd70dc1500f441239201e445ba9ffa2
nohhow <xksrma97@gmail.com>
2022-08-24 23:18:16 +0900

    [💄style] : todoList에서 입력 버튼 마우스 오버시 손가락 모양나오도록 수정 및 width값 수정

--- src/components/TodoList/Form.js
+++ src/components/TodoList/Form.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Form({ value, setValue, handleSubmit }) {
-
   const handleChange = (e) => {
     setValue(e.target.value);
   };
@@ -12,16 +11,17 @@ export default function Form({ value, setValue, handleSubmit }) {
         <input
           type="text"
           name="value"
-          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+          className="w-5/6 px-3 py-2 mr-2 text-gray-500 border rounded shadow"
           placeholder="해야 할 일을 입력하세요."
           value={value}
           onChange={handleChange}
         />
-        <input
-          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
+        <button
+          className="w-1/6 p-1 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
           type="submit"
-          value="입력"
-        />
+        >
+          입력
+        </button>
       </form>
     </div>
   );
