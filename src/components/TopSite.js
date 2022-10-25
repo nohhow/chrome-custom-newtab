c7158963afba9546da76d05f64f428b44e8f5e29
nohhow <jinhyeon.noh@gmail.com>
2022-10-25 22:53:18 +0900

    [💄style] : topSite grid 적용

--- src/components/TopSite.js
+++ src/components/TopSite.js
@@ -13,11 +13,13 @@ function TopSite() {
   return (
     <div className="w-96 p-6 bg-white rounded shadow-sm m-4">
       <h1 className="mb-3">자주 방문한 사이트</h1>
+      <div class="grid grid-cols-2 gap-4">
       {sites.map((data, index) =>(
-        <div key={index}>
-            <a href={data.url}><span className="highlight">{data.title}</span></a>
+          <div key={index}>
+            <a href={data.url}><span className="highlight">{data.title.length > 10 ? data.title.substr(0, 10)+"..." : data.title}</span></a>
         </div>
       ))}
+      </div>
     </div>
   );
 }
