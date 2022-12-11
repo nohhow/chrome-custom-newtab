146064548b9173e95d31cfdb76d18097c89e8cf6
nohhow <jinhyeon.noh@gmail.com>
2022-12-12 00:51:23 +0900

    [🐛bug] : TopSite 이벤트 버블링 현상 해결(click 구분)

--- src/components/Shortcut.js
+++ src/components/Shortcut.js
@@ -27,6 +27,19 @@ function Shortcut() {
     setIndex(e.target.id);
     setModalOpen(true);
   }
+
+  const handleMouseOver = (e) => {
+    const el = e.target;
+    el.classList.remove("opacity-0");
+    el.classList.add("opacity-100");
+  }
+  
+  const handleMouseOut = (e) => {
+    const el = e.target;
+    el.classList.remove("opacity-100");
+    el.classList.add("opacity-0");
+  }
+
   
   const handleSiteOpen = (e) => {
     window.location.href = sites[e.target.id];
@@ -41,19 +54,23 @@ function Shortcut() {
   const handleImgErr = (e) =>{
     e.target.src = bookmarkImg;
   }
+
   return (
     <div className="w-96 p-6 bg-white rounded shadow-sm m-4">
       <h1 className="mb-3">바로 가기</h1>
       <div className="flex justify-between">
-        <button className="shortcut-button rounded-xl" onClick={(e)=>handleEditOpen(e)} id="0">
-            <img className="shortcut-img mx-auto" onClick={(e)=>handleSiteOpen(e)} src={sites[0] ? setSrc(sites[0]):favicon} onError={handleImgErr} alt="shortcut-button" id="0" />
-        </button>
-        <button className="shortcut-button rounded-xl"  onClick={(e)=>handleEditOpen(e)} id="1">
-            <img className="shortcut-img mx-auto" onClick={(e)=>handleSiteOpen(e)} src={sites[1] ? setSrc(sites[1]):favicon} onError={handleImgErr} alt="shortcut-button" id="1" />
-        </button>
-        <button className="shortcut-button rounded-xl"  onClick={(e)=>handleEditOpen(e)} id="2">
-            <img className="shortcut-img mx-auto" onClick={(e)=>handleSiteOpen(e)} src={sites[2] ? setSrc(sites[2]):favicon} onError={handleImgErr} alt="shortcut-button" id="2" />
-        </button>
+        <div className="shortcut-button relative rounded-xl">
+            <button className="absolute opacity-0 right-3 text-xs" onMouseOver={(e)=>handleMouseOver(e)} onMouseOut={(e)=>handleMouseOut(e)} onClick={(e)=>handleEditOpen(e)} id="0">⚙️</button>
+            <img className="shortcut-img mx-auto cursor-pointer" onClick={(e)=>handleSiteOpen(e)} src={sites[0] ? setSrc(sites[0]):favicon} onError={handleImgErr} alt="shortcut-button" id="0" />
+        </div>
+        <div className="shortcut-button relative rounded-xl">
+            <button className="absolute opacity-0 right-3 text-xs" onMouseOver={(e)=>handleMouseOver(e)} onMouseOut={(e)=>handleMouseOut(e)} onClick={(e)=>handleEditOpen(e)} id="1">⚙️</button>
+            <img className="shortcut-img mx-auto cursor-pointer"  onClick={(e)=>handleSiteOpen(e)} src={sites[1] ? setSrc(sites[1]):favicon} onError={handleImgErr} alt="shortcut-button" id="1" />
+        </div>
+        <div className="shortcut-button relative rounded-xl">
+            <button className="absolute opacity-0 right-3 text-xs" onMouseOver={(e)=>handleMouseOver(e)} onMouseOut={(e)=>handleMouseOut(e)} onClick={(e)=>handleEditOpen(e)} id="2">⚙️</button>
+            <img className="shortcut-img mx-auto cursor-pointer" onClick={(e)=>handleSiteOpen(e)} src={sites[2] ? setSrc(sites[2]):favicon} onError={handleImgErr} alt="shortcut-button" id="2" />
+        </div>
       </div>
       {modalOpen ? (
         <ShortcutModal handleSubmit={handleSubmit} value={value} setValue={setValue} setModalOpen={setModalOpen}/>
